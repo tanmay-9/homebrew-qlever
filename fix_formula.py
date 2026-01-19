20370ebe14777eb6c61ab5d59ae90212ad9aa0ca
Tanmay Garg <tanmaygarg9879@gmail.com>
2026-01-19 17:48:55 +0100

    Use ruff on fix_formula.py

--- fix_formula.py
+++ fix_formula.py
@@ -8,24 +8,20 @@ formula_path = Path("Formula/qlever-control.rb")
 content = formula_path.read_text()
 
 # Rename class from Qlever to QleverControl
-content = re.sub(
-    r"class Qlever <",
-    "class QleverControl <",
-    content
-)
+content = re.sub(r"class Qlever <", "class QleverControl <", content)
 
 # Fix description
 content = re.sub(
     r'desc "Shiny new formula"',
     'desc "Command-line tool for QLever graph database"',
-    content
+    content,
 )
 
 # Fix homepage
 content = re.sub(
     r'homepage ".*?"',
     'homepage "https://github.com/ad-freiburg/qlever-control"',
-    content
+    content,
 )
 
 # Add license after homepage if not present
@@ -33,15 +29,15 @@ if 'license "' not in content:
     content = re.sub(
         r'(homepage "https://github.com/ad-freiburg/qlever-control")',
         r'\1\n  license "Apache-2.0"',
-        content
+        content,
     )
 
 # Fix test block (poet generates "test do\n    false\n  end")
 content = re.sub(
-    r'test do.*?end',
+    r"test do.*?end",
     'test do\n    assert_match version.to_s, shell_output("#{bin}/qlever --version")\n  end',
     content,
-    flags=re.DOTALL
+    flags=re.DOTALL,
 )
 
 formula_path.write_text(content)
